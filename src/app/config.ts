099d9d382da702a7f88b8da353e49b849a28ed23
Eetu Lepistö <bodom02@gmail.com>
2023-03-24 16:09:51 +0200

    Added authentication provider

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,16 +1,29 @@
-import { cleanEnv, url } from "envalid";
+import { cleanEnv, str, url } from "envalid";
 
 type Config = {
+  auth: {
+    url: string;
+    realm: string;
+    clientId: string;
+  };
   api: {
     baseUrl: string;
   };
 };
 
 const env = cleanEnv(import.meta.env, {
+  VITE_KEYCLOAK_URL: url(),
+  VITE_KEYCLOAK_REALM: str(),
+  VITE_KEYCLOAK_CLIENT_ID: str(),
   VITE_API_BASE_URL: url()
 });
 
 const config: Config = {
+  auth: {
+    url: env.VITE_KEYCLOAK_URL,
+    realm: env.VITE_KEYCLOAK_REALM,
+    clientId: env.VITE_KEYCLOAK_CLIENT_ID
+  },
   api: {
     baseUrl: env.VITE_API_BASE_URL
   }

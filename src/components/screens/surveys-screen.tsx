aa64ce74f109cea1751b271273d8dd6ddec32b15
Jdallos <joshdallos@gmail.com>
2023-04-17 16:08:45 +0300

    Surveys screen boilerplate

--- src/components/screens/surveys-screen.tsx
+++ src/components/screens/surveys-screen.tsx
@@ -1,12 +1,42 @@
-import strings from "../../localization/strings";
+import { Box, Button, InputLabel, Select, Stack, TextField } from "@mui/material";
 
 /**
  * Renders surveys screen
  */
 const SurveysScreen = () => {
+
+  /**
+   * Creates a new survey
+   */
+  const createSurvey = () => {
+    console.log("TODO");
+  }
+
   return (
-    <div>{strings.generic.notImplemented}</div>
-  )
+    <Stack direction="row" sx={{ backgroundColor: "white" }}>
+      <Box>
+        <InputLabel>Näyta</InputLabel>
+        <Select />
+      </Box>
+      <Box>
+        <InputLabel>Järjestä</InputLabel>
+        <Select />
+      </Box>
+      <Box>
+        <InputLabel>Kategoria</InputLabel>
+        <Select />
+      </Box>
+      <Box>
+        <InputLabel>Suodata</InputLabel>
+        <Select />
+      </Box>
+      <Box>
+        <InputLabel>Suodata</InputLabel>
+        <TextField />
+      </Box>
+      <Button onClick={ createSurvey }>Luo uusi kysely</Button>
+    </Stack>
+  );
 };
 
 export default SurveysScreen;

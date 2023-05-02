e0cdc067952b52f6eb001ba2526049c1e3ad4d44
Jdallos <joshdallos@gmail.com>
2023-05-02 11:49:17 +0300

    New question option functionality in panel

--- src/components/layout-components/page-properties.tsx
+++ src/components/layout-components/page-properties.tsx
@@ -3,11 +3,40 @@ import strings from "../../localization/strings";
 import { Edit } from "@mui/icons-material";
 import { QuestionType } from "../../types";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { ChangeEvent, useState } from "react";
+import { v4 as uuid } from 'uuid';
 
+// TODO: This type will come from generated files.
+type QuestionOptions = {
+  id: string;
+  question: string;
+}
 /**
  * Renders page properties component
  */
 const PageProperties = () => {
+  // TODO: Populated with options from the backend and debounce
+  const [ questionOptions, setQuestionOptions ] = useState<QuestionOptions[]>();
+
+  /**
+   * Handle question option change
+   */
+  const handleQuestionOptionChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, id: string) => {
+    if (!questionOptions) return;
+
+    const updatedOptions = questionOptions.map(option => {
+      if (option.id === id) {
+        return {...option, question: event.target.value}
+      }
+      return option;
+    });
+
+    // TODO: Debounce with backend
+    setQuestionOptions(updatedOptions);
+  };
+
+  const newQuestionOption = { id: uuid(), question: "" }
+
   return (
     <>
       <Box p={2} sx={{ borderBottom: "1px solid #DADCDE" }}>
@@ -67,12 +96,34 @@ const PageProperties = () => {
           </MenuItem>
         </TextField>
       </Box>
+      { questionOptions &&
+        <Box p={2} sx={{ borderBottom: "1px solid #DADCDE" }}>
+          { questionOptions?.map(option =>
+            <TextField
+              key={option.id}
+              fullWidth
+              multiline
+              value={ option.question }
+              name={ option.question }
+              onChange={ (e) => handleQuestionOptionChange(e, option.id) }
+              placeholder={strings.editSurveysScreen.editPagesPanel.title}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <Edit fontSize="small" color="primary" />
+                  </InputAdornment>
+                )
+              }}
+            />
+          )}
+        </Box>
+      }
       <Box>
         <Button
           size="large"
           variant="contained"
           startIcon={ <AddCircleIcon /> }
-          onClick={ () => console.log("TODO") }
+          onClick={ () => questionOptions ? setQuestionOptions([...questionOptions, newQuestionOption ]) : setQuestionOptions([newQuestionOption]) }
         >
           { strings.editSurveysScreen.editPagesPanel.addOption }
         </Button>

5ea9ba048b328744441adc4cba0de26369b3a33d
Jdallos <joshdallos@gmail.com>
2023-05-02 15:40:06 +0300

    Minimal styles for "rough" templates

--- src/components/pages/templates/query-template-EXAMPLE.ts
+++ src/components/pages/templates/query-template-EXAMPLE.ts
@@ -1,15 +1,24 @@
 import strings from "../../../localization/strings";
-import { Question } from "../../../types";
+import sanitizeHtml, { IOptions } from "sanitize-html";
+
+// TODO: This may want refining
+/**
+ * Options for html sanitizer
+ */
+const sanitizeOptions: IOptions = {
+  allowedTags: false,
+  allowedAttributes: false
+};
 
 // TODO: This template needs styling
-const titleAndQueryTemplate = (question?: Question) => {
+const titleAndQueryTemplate = (questions?: string) => {
 
-const questionContent = question || strings.editSurveysScreen.pages.noQuestion;
+const questionContent = questions || strings.editSurveysScreen.pages.noQuestion;
 
 return (
-    `<div style=" background: #00AA46; color: #ffffff; height: inherit; align-items: center; display: flex; flex-direction: column;">
-      <h1 style="margin: 0; padding: 0; text-transform: uppercase;">Kiitos äänestäsi!</h1>
-      <div>${ questionContent }</div>
+    `<div style="background: #00AA46; color: #ffffff; height: 3840px; width: 100%; align-items: center; display: flex; flex-direction: column;">
+      <h1 style="margin: 0; padding: 0; text-transform: uppercase; font-size: 20rem">Kiitos äänestäsi!</h1>
+      <div style="width: 100%;">${ sanitizeHtml(questionContent, sanitizeOptions) }</div>
     </div>`
   )
 };

3eb5e2076335e2daf4fa4bb3aa5b0d20c713cd23
Jdallos <joshdallos@gmail.com>
2023-05-02 10:19:47 +0300

    Remove message  event listener from window

--- src/components/layout-components/preview.tsx
+++ src/components/layout-components/preview.tsx
@@ -16,18 +16,26 @@ interface Props {
  * Renders preview component
  */
 const Preview = ({ htmlString, width, height, scale, onPanelPropertiesChange }: Props) => {
-
   /**
    * Set up event listener to recieve post message from iframe
    */
   useEffect(() => {
-    window.addEventListener("message", (event) => {
-      // TODO: This could be secured using the origin from postMessage?
-      if (event.data === "iFrameClick") {
-        onPanelPropertiesChange();
-      }
-    });
+    window.addEventListener("message", handlePostMessageEventListener);
+
+    return () => window.removeEventListener("message", handlePostMessageEventListener);
   },[]);
+
+  /**
+   * Handles post messsage event from iframe
+   *
+   * @param event message event
+   */
+  const handlePostMessageEventListener = (event: MessageEvent) => {
+    // TODO: This could be secured using the origin from postMessage?
+    if (event.data === "iFrameClick") {
+      onPanelPropertiesChange();
+    }
+  };
 
   /**
    * Parse HTML string to dom element

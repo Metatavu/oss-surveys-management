22120641f94d9f524add2684c628ac86a464fbc5
Tuomas Kämppi <tuomas.kamppi@metatavu.fi>
2023-05-11 09:59:59 +0300

    template wrapper styles

--- src/components/pages/templates/title-and-question.ts
+++ src/components/pages/templates/title-and-question.ts
@@ -1,7 +1,12 @@
-// TODO: This template needs styling
-const titleAndQuestionTemplate = `<div style="background: #00AA46; color: #ffffff; height: 3840px; width: 100%; align-items: center; display: flex; flex-direction: column;">
-    <h1 style="margin: 0; padding: 0; text-transform: uppercase; font-size: 20rem">Kiitos äänestäsi!</h1>
-    <div style="width: 100%;"><div data-component="question"></div></div>
+const titleAndQuestionTemplate = `
+  <div style="height: 100vh; width: 100vw; background-color: #00AA46; color: #ffffff; display: flex; flex: 1; flex-direction: column; padding: 10%; box-sizing: border-box;">
+    <div style="display: flex; flex: 1; flex-direction: column;">
+      <h1 style="margin: 0; padding: 0; text-transform: uppercase; font-size: 12rem; font-family: SBonusDisplay-Black;">Otsikko</h1>
+      <div style="width: 100%;">
+        <div data-component="question"></div>
+      </div>
+    /div>
+    <button style="width: 100%; background-color: transparent; border: 20px solid #ffffff; color: #ffffff; height: 250px; font-family: SBonusText-Bold; font-size: 6rem;">Seuraava</button>
   </div>`;
 
 export default titleAndQuestionTemplate;

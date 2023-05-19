c9fc5fd61f32977d426013bbd1c10dcb7292c01f
Ville Juutila <ville.juutila@gmail.com>
2023-05-19 08:30:23 +0300

    Added generic loader and ability to delete page

---
+++ src/components/generic/loader-wrapper.tsx
@@ -0,0 +1,28 @@
+import { Backdrop, CircularProgress } from "@mui/material";
+
+/**
+ * Components properties
+ */
+interface Props {
+  loading: boolean;
+  children: React.ReactNode;
+}
+
+/**
+ * Loader wrapper component
+ */
+const LoaderWrapper = ({ loading, children }: Props) => {
+  if (loading)
+    return (
+      <>
+        <Backdrop sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }} open={loading}>
+          <CircularProgress color="inherit" />
+        </Backdrop>
+        {children}
+      </>
+    );
+
+  return children;
+};
+
+export default LoaderWrapper;

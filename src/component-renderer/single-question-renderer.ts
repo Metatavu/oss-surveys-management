5f89549917d2738cae2dad07e575ef35cdf29c65
Ville Juutila <ville.juutila@gmail.com>
2023-05-25 16:54:00 +0300

    Removed inline styling from renderers

--- src/component-renderer/single-question-renderer.ts
+++ src/component-renderer/single-question-renderer.ts
@@ -12,19 +12,5 @@ export class SingleSelectTextQuestionRenderer extends AbstractQuestionRenderer {
    * @returns html string
    */
   public render = (option: string): string =>
-    `<button
-      id="${uuid()}"
-      style="
-        width: 100%;
-        height: 250px;
-        font-size: 6rem;
-        font-family: 'SBonusText-Bold';
-        color: #fff;
-        background: transparent;
-        border: 20px solid #fff;
-        margin-bottom: 6rem;
-      "
-    >
-      ${option}
-    </button>`;
+    `<button class="option" style="margin-bottom: 3rem;">${option}</button>`;
 }
--- src/component-renderer/text-renderer.ts
+++ src/component-renderer/text-renderer.ts
@@ -11,18 +11,5 @@ export class TextRenderer extends AbstractQuestionRenderer {
    * @param text Text
    * @returns html string
    */
-  public render = (text: string): string =>
-    `<p
-      id="${uuid()}"
-      style="
-        margin: 0;
-        padding: 0;
-        line-height: 150%;
-        font-size: 8rem;
-        white-space: pre-line;
-        font-family: SBonusDisplay-Regular;
-      "
-    >
-      ${text}
-    </p>`;
+  public render = (text: string): string => `<p>${text}</p>`;
 }

430f1b1fd51252871cbaca5b71d6930aa3d1168e
Tuomas Kämppi <tuomas.kamppi@metatavu.fi>
2023-05-25 08:57:20 +0300

    HD resolution

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,9 +4,9 @@ export const EDITOR_SCREEN_PREVIEW_CONTAINER_WIDTH = 280;
 
 export const EDITOR_SCREEN_PREVIEW_CONTAINER_HEIGHT = 497;
 
-export const DEVICE_WIDTH = 2160;
+export const DEVICE_WIDTH = 1080;
 
-export const DEVICE_HEIGHT = 3840;
+export const DEVICE_HEIGHT = 1920;
 
 export const EDITABLE_TEXT_PAGE_ELEMENTS = [PageElementType.H1, PageElementType.P];
 

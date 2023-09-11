e03bac3a0c89673a5911d345bb40f2b71168b2ea
Jdallos <joshdallos@gmail.com>
2023-09-11 11:05:55 +0300

    Add line breaks to paragraphs

--- src/component-renderer/paragraph-renderer.ts
+++ src/component-renderer/paragraph-renderer.ts
@@ -5,10 +5,22 @@ import { AbstractRenderer } from "./abstract-renderer";
  */
 export class ParagraphRenderer extends AbstractRenderer {
   /**
-   * Renders an HTML paragraph element with given value as its text.
+   * Renders a dev element containing given value split by line breaks as p elements.
    *
    * @param value display text
    * @returns html string
    */
-  public render = (value: string): string => `<p>${value}</p>`;
+  public render = (value: string): string => {
+    const lines = value.split("\n");
+    const pTags = lines
+      .map((line) => {
+        if (line.trim() === "") return "<p>&nbsp;</p>";
+        // TODO: Using above as br not working in the device?
+        // if (line.trim() === "") return "<br/>";
+
+        return `<p>${line}</p>`;
+      })
+      .join("\n");
+    return `<div>${pTags}</div>`;
+  };
 }
--- src/component-renderer/title-renderer.ts
+++ src/component-renderer/title-renderer.ts
@@ -5,7 +5,7 @@ import { AbstractRenderer } from "./abstract-renderer";
  */
 export class TitleRenderer extends AbstractRenderer {
   /**
-   * Renders a div element with given value as its h1 contents.
+   * Renders a div element with given value split by line breaks as its h1 elements.
    *
    * @param value display text
    * @returns html string
@@ -14,7 +14,9 @@ export class TitleRenderer extends AbstractRenderer {
     const lines = value.split("\n");
     const h1Tags = lines
       .map((line) => {
-        if (line.trim() === "") return "<br/>";
+        if (line.trim() === "") return "<h1>&nbsp;</h1>";
+        // TODO: Using the above a BR not working on device?
+        // if (line.trim() === "") return "<br/>";
 
         return `<h1 class="md">${line}</h1>`;
       })
